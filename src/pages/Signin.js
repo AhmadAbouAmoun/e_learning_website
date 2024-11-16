ae79412ebbf8b4de99a2b79d56da71acddb33113
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-16 12:30:40 +0200

    created the JSX in the Signin component

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,4 +1,27 @@
-const Login = () => {
-    return <div></div>;
+import Input from "../components/Input";
+import SiginButton from "../components/SigininButton";
+
+const Signin = () => {
+    return (
+        <div>
+            <form id="signupForm" class="form_container">
+                <div className="logo_container">
+                    <img src="logo.png" alt="" />
+                </div>
+                <div className="title_container">
+                    <p className="title">Login to your Accounr</p>
+                    <span className="subtitle">
+                        Get started with our app, just create an account and enjoy the experience.
+                    </span>
+                </div>
+                <Input />
+                <Input />
+                <SiginButton />
+                <button title="Sign In" type="submit" className="sign-in_btn" onClick={() => {}}>
+                    <span>Do not have an account? Sign up</span>
+                </button>
+            </form>
+        </div>
+    );
 };
-export default Login;
+export default Signin;
