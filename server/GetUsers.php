f663425656cbb94a39790bf534ff09226ca29c0e
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-19 00:49:33 +0200

    created the GetUsers api

---
+++ server/GetUsers.php
@@ -0,0 +1,27 @@
+<?php
+
+include "connection.php";
+
+if(!$_POST["type"]){
+    echo"something is not right";
+    exit;
+    }
+$type=$_POST["type"];
+
+$query = $connection->prepare("SELECT * FROM $type");
+$query->execute();
+
+$result = $query->get_result();
+
+$users = [];
+if ($result->num_rows > 0) {
+    while ($user = $result->fetch_assoc()) {
+        $users[] = $user; 
+    }
+    echo json_encode($users);
+} else {
+    echo json_encode([
+        "message" => "Not Found"
+    ]);
+}
+?>
