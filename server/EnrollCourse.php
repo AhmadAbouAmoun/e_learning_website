e9428ca56b880215dbe84524aa8484325d867309
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-19 01:36:13 +0200

    implemented the Enroll Course

--- server/EnrollCourse.php
+++ server/EnrollCourse.php
@@ -1,3 +1,38 @@
 <?php
 include "connection.php";
 include "JWT.php";
+
+if(!$_POST["course_id"]){
+    echo"cousre id was not provided";
+    return;
+}
+$course_id=$_POST["course_id"];
+if (isset($_POST['token'])) {
+    $token = $_POST['token'];
+    if(verifyJWT($token,$secret))
+    {
+        $user_id=getJWTValue($token,"user_id");
+        echo"$user_id";
+        $type=getJWTValue($token,"type");
+        echo"<br>"."$type"."<br>";		
+        $query=$connection->prepare("INSERT INTO enrolledcourses(student_id,course_id) VALUES (?,?)");
+        if(!$query){
+            echo"issue with the query ".$connection->error;
+            exit;
+        }
+        $query->bind_param("ii",$course_id,$user_id);
+        if($query->execute()){
+            echo json_encode(["message"=>"successfully enrolled","status"=>"success"]);
+        }
+    }
+    else{
+        echo"error with token";
+        return;
+    }
+
+}
+else{
+    echo"token was not provided"."<br>";
+    return;
+
+}
