32d5c450cf334e186fac817d58b10c75ede994fd
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-19 00:45:09 +0200

    created the CreateInstructor api

---
+++ server/CreateInstructor.php
@@ -0,0 +1,52 @@
+<?php
+include "connection.php";
+include "JWT.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if(!$_POST["name"]||!$_POST["email"]||!$_POST["password"]){
+echo"something is not right";
+exit;
+}
+$name=$_POST["name"];
+$email=$_POST["email"];
+$password=$_POST["password"];
+$banned=false;
+
+$hash_password=password_hash($password,PASSWORD_DEFAULT);
+
+$check=$connection->prepare("SELECT email FROM teacher WHERE email=?");
+$check->bind_param("s",  $email);
+$check->execute();
+$result=$check->get_result();
+if($result->num_rows>0){
+    echo"account already exists ";
+    exit;
+}
+else{
+    $query=$connection->prepare("INSERT INTO teacher(name,email,password,banned) VALUES (?,?,?,?)");
+if(!$query){
+    echo"issue with the query ".$connection->error;
+    exit;
+}
+$query->bind_param("sssi", $name, $email, $hash_password,$banned);
+
+if($query->execute()){
+    $user_id = $connection->insert_id;
+    $payload = ['user_id' => $user_id, 'type' => "teacher"];
+    $jwt=createJWT($header,$payload,$secret);
+    $response=["token"=>$jwt,
+    "status" => "success",
+];
+    echo json_encode($response);
+}
+else{
+    $response=[
+    "status" => "failed",
+    "message"=> "error ".$connection->error
+];
+}
+$query->close();
+$connection->close(); 
+exit;
+}
