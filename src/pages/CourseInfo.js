3c9826bc98b04933c77ea56df784614df530d889
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-21 05:10:21 +0200

    created the navbat in CourseInfo

--- src/pages/CourseInfo.js
+++ src/pages/CourseInfo.js
@@ -1,7 +1,24 @@
+import {useState} from "react";
 import {useParams} from "react-router-dom";
 
 const CourseInfo = () => {
-    const {courseId} = useParams();
-    return <h1>HELLO</h1>;
+    const {id} = useParams();
+    const [path, setPath] = useState("Announcments");
+    console.log(path);
+
+    return (
+        <div>
+            <nav className="navbar">
+                <h3>Website Name</h3>
+
+                <ul>
+                    <li onClick={() => setPath("Announcments")}>Announcments</li>
+                    <li onClick={() => setPath("Assignments")}>Assignments</li>
+                </ul>
+
+                <span>Welcome</span>
+            </nav>
+        </div>
+    );
 };
 export default CourseInfo;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,42 +2,49 @@ import {useState} from "react";
 import Courses from "../components/Courses";
 import "../styles/navbar.css";
 import EnrolledCourses from "../components/EnrolledCourses";
-import {Route, Routes, useNavigate} from "react-router-dom";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
 import CourseInfo from "./CourseInfo";
 const HomePage = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const isPath = location.pathname.includes("course-info");
+
     return (
-        <div>
-            <nav className="navbar">
-                <h3>Website Name</h3>
+        <>
+            {!isPath && (
+                <div>
+                    <nav className="navbar">
+                        <h3>Website Name</h3>
 
-                <ul>
-                    <li
-                        onClick={() => {
-                            navigate("Courses");
-                        }}
-                    >
-                        Courses{" "}
-                    </li>
-                    <li
-                        onClick={() => {
-                            navigate("Enrolled");
-                        }}
-                    >
-                        Enrolled Courses
-                    </li>
-                    <li onClick={() => {}}>Invitations</li>
-                </ul>
+                        <ul>
+                            <li
+                                onClick={() => {
+                                    navigate("Courses");
+                                }}
+                            >
+                                Courses{" "}
+                            </li>
+                            <li
+                                onClick={() => {
+                                    navigate("Enrolled");
+                                }}
+                            >
+                                Enrolled Courses
+                            </li>
+                            <li onClick={() => {}}>Invitations</li>
+                        </ul>
 
-                <span>Welcome</span>
-            </nav>
+                        <span>Welcome</span>
+                    </nav>
+                </div>
+            )}
             <Routes>
                 <Route path="Courses" element={<Courses />} />
                 <Route path="Enrolled/*" element={<EnrolledCourses />}>
                     <Route path="course-info/:id" element={<CourseInfo />} />
                 </Route>
             </Routes>
-        </div>
+        </>
     );
 };
 export default HomePage;
